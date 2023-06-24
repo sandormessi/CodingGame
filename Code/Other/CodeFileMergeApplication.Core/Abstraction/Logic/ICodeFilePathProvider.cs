c772432ef581714b02224b0a66ef6a0d974ed323
Sándor Ács <sandormessi@hotmail.com>
2023-06-24 15:00:27 +0200

    CodeFile paths provider

---
+++ Code/Other/CodeFileMergeApplication.Core/Abstraction/Logic/ICodeFilePathProvider.cs
@@ -0,0 +1,12 @@
+﻿namespace CodeFileMergeApplication.Core.Abstraction.Logic;
+
+using System.Collections.Generic;
+
+public interface ICodeFilePathProvider
+{
+   #region Public Methods and Operators
+
+   IEnumerable<string> GetCodeFilePaths();
+
+   #endregion
+}

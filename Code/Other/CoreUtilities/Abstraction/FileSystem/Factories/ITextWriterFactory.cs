0130d852ee6e85fe84513506252ac618ae285b64
Sándor Ács <sandormessi@hotmail.com>
2023-06-24 13:30:07 +0200

    Add TextWriterFactory

---
+++ Code/Other/CoreUtilities/Abstraction/FileSystem/Factories/ITextWriterFactory.cs
@@ -0,0 +1,15 @@
+﻿namespace CoreUtilities.Abstraction.FileSystem.Factories;
+
+using System.IO;
+using System.Text;
+
+public interface ITextWriterFactory
+{
+   #region Public Methods and Operators
+
+   TextWriter CreateTextWriter(Stream baseStream, Encoding encoding);
+
+   TextWriter CreateTextWriter(Stream baseStream);
+
+   #endregion
+}
---
+++ Code/Other/CoreUtilities/Implementation/FileSystem/Factories/TextWriterFactory.cs
@@ -0,0 +1,34 @@
+﻿namespace CoreUtilities.Implementation.FileSystem.Factories;
+
+using System;
+using System.IO;
+using System.Text;
+
+using CoreUtilities.Abstraction.FileSystem.Factories;
+
+public class TextWriterFactory : ITextWriterFactory
+{
+   #region ITextWriterFactory Members
+
+   public TextWriter CreateTextWriter(Stream baseStream, Encoding encoding)
+   {
+      if (baseStream == null)
+      {
+         throw new ArgumentNullException(nameof(baseStream));
+      }
+
+      if (encoding == null)
+      {
+         throw new ArgumentNullException(nameof(encoding));
+      }
+
+      return new StreamWriter(baseStream, encoding);
+   }
+
+   public TextWriter CreateTextWriter(Stream baseStream)
+   {
+      return CreateTextWriter(baseStream, Encoding.UTF8);
+   }
+
+   #endregion
+}

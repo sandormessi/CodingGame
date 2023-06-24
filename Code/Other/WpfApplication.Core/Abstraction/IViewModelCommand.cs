9e0f79020626c5ce1ffdffb519fac30feabb51fa
Sándor Ács <sandormessi@hotmail.com>
2023-06-24 22:53:41 +0200

    Add command base

---
+++ Code/Other/WpfApplication.Core/Abstraction/IViewModelCommand.cs
@@ -0,0 +1,12 @@
+﻿namespace WpfApplication.Core.Abstraction;
+
+using System.Windows.Input;
+
+public interface IViewModelCommand : ICommand
+{
+   #region Public Methods and Operators
+
+   void Update();
+
+   #endregion
+}
---
+++ Code/Other/WpfApplication.Core/Implementation/CommandBase.cs
@@ -0,0 +1,49 @@
+﻿namespace WpfApplication.Core.Implementation;
+
+using System;
+
+using WpfApplication.Core.Abstraction;
+
+public abstract class CommandBase : IViewModelCommand
+{
+   #region Public Events
+
+   public event EventHandler? CanExecuteChanged;
+
+   #endregion
+
+   #region IViewModelCommand Members
+
+   public bool CanExecute(object? parameter)
+   {
+      return DoCanExecute(parameter);
+   }
+
+   public void Execute(object? parameter)
+   {
+      DoExecute(parameter);
+   }
+
+   public void Update()
+   {
+      throw new NotImplementedException();
+   }
+
+   #endregion
+
+   #region Methods
+
+   protected virtual bool DoCanExecute(object? parameter)
+   {
+      return true;
+   }
+
+   protected abstract void DoExecute(object? parameter);
+
+   protected virtual void OnCanExecuteChanged()
+   {
+      CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+   }
+
+   #endregion
+}

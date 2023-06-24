e0c5fbf2ce8078ebdc746db5f44acd612727b870
Sándor Ács <sandormessi@hotmail.com>
2023-06-24 15:46:45 +0200

    Add CodeFileReader (not ready)

---
+++ Code/Other/CodeFileMergeApplication.Core/Abstraction/Logic/ICodeFileReader.cs
@@ -0,0 +1,25 @@
+﻿namespace CodeFileMergeApplication.Core.Abstraction.Logic;
+
+using System.Threading;
+using System.Threading.Tasks;
+
+using CodeFileMergeApplication.Core.Abstraction.Data;
+
+public interface ICodeFileReader
+{
+   #region Public Methods and Operators
+
+   CodeFile ReadFromFile(string filePath);
+
+   Task<CodeFile> ReadFromFileAsync(string filePath);
+
+   Task<CodeFile> ReadFromFileAsync(string filePath, CancellationToken cancellationToken);
+
+   CodeFile ReadFromString(string fileContent);
+
+   Task<CodeFile> ReadFromStringAsync(string fileContent);
+
+   Task<CodeFile> ReadFromStringAsync(string fileContent, CancellationToken cancellationToken);
+
+   #endregion
+}
---
+++ Code/Other/CodeFileMergeApplication.Core/Implementation/Logic/CodeFileReader.cs
@@ -0,0 +1,86 @@
+﻿namespace CodeFileMergeApplication.Core.Implementation.Logic;
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+using CodeFileMergeApplication.Core.Abstraction.Data;
+using CodeFileMergeApplication.Core.Abstraction.Logic;
+
+using CoreUtilities.Abstraction.FileSystem;
+
+public class CodeFileReader : ICodeFileReader
+{
+   #region Constants and Fields
+
+   private readonly IFileReader fileReader;
+
+   #endregion
+
+   #region Constructors and Destructors
+
+   public CodeFileReader(IFileReader fileReader)
+   {
+      this.fileReader = fileReader ?? throw new ArgumentNullException(nameof(fileReader));
+   }
+
+   #endregion
+
+   #region ICodeFileReader Members
+
+   public CodeFile ReadFromFile(string filePath)
+   {
+      if (string.IsNullOrWhiteSpace(filePath))
+      {
+         throw new ArgumentException("Value cannot be null or whitespace.", nameof(filePath));
+      }
+
+      var fileContent = fileReader.ReadAllTextFromFile(filePath);
+
+      return ReadFromString(fileContent);
+   }
+
+   public CodeFile ReadFromString(string fileContent)
+   {
+      if (string.IsNullOrWhiteSpace(fileContent))
+      {
+         throw new ArgumentException("Value cannot be null or whitespace.", nameof(fileContent));
+      }
+
+      throw new NotImplementedException();
+   }
+
+   public Task<CodeFile> ReadFromStringAsync(string fileContent, CancellationToken cancellationToken)
+   {
+      if (string.IsNullOrWhiteSpace(fileContent))
+      {
+         throw new ArgumentException("Value cannot be null or whitespace.", nameof(fileContent));
+      }
+
+      return Task.Factory.StartNew(() => ReadFromString(fileContent), cancellationToken);
+   }
+
+   public Task<CodeFile> ReadFromFileAsync(string filePath)
+   {
+      return ReadFromFileAsync(filePath, CancellationToken.None);
+   }
+
+   public Task<CodeFile> ReadFromStringAsync(string fileContent)
+   {
+      return ReadFromStringAsync(fileContent, CancellationToken.None);
+   }
+
+   public async Task<CodeFile> ReadFromFileAsync(string filePath, CancellationToken cancellationToken)
+   {
+      if (string.IsNullOrWhiteSpace(filePath))
+      {
+         throw new ArgumentException("Value cannot be null or whitespace.", nameof(filePath));
+      }
+
+      var fileContent = await fileReader.ReadAllTextFromFileAsync(filePath, cancellationToken).ConfigureAwait(false);
+
+      return await ReadFromStringAsync(fileContent, cancellationToken).ConfigureAwait(false);
+   }
+
+   #endregion
+}

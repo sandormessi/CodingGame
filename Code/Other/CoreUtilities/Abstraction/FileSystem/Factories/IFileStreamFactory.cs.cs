014ba042c08d6107f2f427cb8217a9ac620ed954
Sándor Ács <sandormessi@hotmail.com>
2023-06-24 13:25:26 +0200

    Move factories to factory folder

---
+++ Code/Other/CoreUtilities/Abstraction/FileSystem/Factories/IFileStreamFactory.cs.cs
@@ -0,0 +1,18 @@
+﻿namespace CoreUtilities.Abstraction.FileSystem.Factories;
+
+using System.IO;
+
+public interface IFileStreamFactory
+{
+    #region Public Methods and Operators
+
+    Stream CreateFileStream(string filePath, FileMode mode, FileAccess access, FileShare share);
+
+    Stream CreateFileStream(string filePath, FileMode mode, FileAccess access);
+
+    Stream CreateFileStream(string filePath, FileMode mode);
+
+    Stream CreateFileStream(string filePath);
+
+    #endregion
+}
---
+++ Code/Other/CoreUtilities/Abstraction/FileSystem/Factories/IStreamReaderFactory.cs
@@ -0,0 +1,15 @@
+﻿namespace CoreUtilities.Abstraction.FileSystem.Factories;
+
+using System.IO;
+using System.Text;
+
+public interface IStreamReaderFactory
+{
+    #region Public Methods and Operators
+
+    TextReader CreateReader(Stream baseStream, Encoding encoding);
+
+    TextReader CreateReader(Stream baseStream);
+
+    #endregion
+}

1d35caf27bfb7c0c1e1e8fb8a1ee239888e9425a
Sándor Ács <sandormessi@hotmail.com>
2023-06-24 15:00:40 +0200

    Namespace extractor interface

---
+++ Code/Other/CodeFileMergeApplication.Core/Abstraction/Logic/INamespaceExtractor.cs
@@ -0,0 +1,12 @@
+﻿namespace CodeFileMergeApplication.Core.Abstraction.Logic;
+
+using CodeFileMergeApplication.Core.Abstraction.Data;
+
+public interface INamespaceExtractor
+{
+   #region Public Methods and Operators
+
+   Namespace GetNamespace(string codeBlockText);
+
+   #endregion
+}

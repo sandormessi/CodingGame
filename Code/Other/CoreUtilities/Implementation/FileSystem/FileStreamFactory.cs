30884f2c080102794e4e45094fca4dc48c00dccb
Sándor Ács <sandormessi@hotmail.com>
2023-06-24 13:19:44 +0200

    Add FileStream factory

---
+++ Code/Other/CoreUtilities/Implementation/FileSystem/FileStreamFactory.cs
@@ -0,0 +1,32 @@
+﻿namespace CoreUtilities.Implementation.FileSystem;
+
+using System.IO;
+
+using CoreUtilities.Abstraction.FileSystem;
+
+public class FileStreamFactory : IFileStreamFactory
+{
+   #region IFileStreamFactory Members
+
+   public Stream CreateFileStream(string filePath, FileMode mode, FileAccess access, FileShare share)
+   {
+      return new FileStream(filePath, mode, access, share);
+   }
+
+   public Stream CreateFileStream(string filePath, FileMode mode, FileAccess access)
+   {
+      return CreateFileStream(filePath, mode, access, FileShare.None);
+   }
+
+   public Stream CreateFileStream(string filePath, FileMode mode)
+   {
+      return CreateFileStream(filePath, mode, FileAccess.Read, FileShare.None);
+   }
+
+   public Stream CreateFileStream(string filePath)
+   {
+      return CreateFileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.None);
+   }
+
+   #endregion
+}

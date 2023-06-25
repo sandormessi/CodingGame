79b28a87c2fea6d986040e9b36253c54837f5df6
Sándor Ács <sandormessi@hotmail.com>
2023-06-25 16:17:17 +0200

    CodeFileWriter interface

---
+++ Code/Other/CodeFileMergeApplication.Core/Abstraction/Logic/ICodeFileWriter.cs
@@ -0,0 +1,19 @@
+﻿namespace CodeFileMergeApplication.Core.Abstraction.Logic;
+
+using System.Threading;
+using System.Threading.Tasks;
+
+using CodeFileMergeApplication.Core.Abstraction.Data;
+
+public interface ICodeFileWriter
+{
+   #region Public Methods and Operators
+
+   void WriteCodeFile(CodeFile codeFile, string targetPath);
+
+   Task WriteCodeFileAsync(CodeFile codeFile, string targetPath);
+
+   Task WriteCodeFileAsync(CodeFile codeFile, string targetPath, CancellationToken cancellationToken);
+
+   #endregion
+}

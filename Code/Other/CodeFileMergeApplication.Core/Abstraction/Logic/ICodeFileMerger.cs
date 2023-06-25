64e6edeb08ef49ffe17f4a365a6274715ed83499
Sándor Ács <sandormessi@hotmail.com>
2023-06-25 16:17:05 +0200

    CodeFileMerger interface

---
+++ Code/Other/CodeFileMergeApplication.Core/Abstraction/Logic/ICodeFileMerger.cs
@@ -0,0 +1,20 @@
+﻿namespace CodeFileMergeApplication.Core.Abstraction.Logic;
+
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+using CodeFileMergeApplication.Core.Abstraction.Data;
+
+public interface ICodeFileMerger
+{
+   #region Public Methods and Operators
+
+   CodeFile MergeCodeFiles(IEnumerable<CodeFile> codeFilesToMerge);
+
+   Task<CodeFile> MergeCodeFilesAsync(IEnumerable<CodeFile> codeFilesToMerge);
+
+   Task<CodeFile> MergeCodeFilesAsync(IEnumerable<CodeFile> codeFilesToMerge, CancellationToken cancellationToken);
+
+   #endregion
+}

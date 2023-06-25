c0a15bef1e532b2c9eb2f20a0c5b14f7d8bc2652
Sándor Ács <sandormessi@hotmail.com>
2023-06-25 18:21:23 +0200

    IUsingCollector interface

---
+++ Code/Other/CodeFileMergeApplication.Core/Abstraction/Logic/IUsingCollector.cs
@@ -0,0 +1,26 @@
+﻿namespace CodeFileMergeApplication.Core.Abstraction.Logic;
+
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+using CodeFileMergeApplication.Core.Abstraction.Data;
+
+public interface IUsingCollector
+{
+   #region Public Methods and Operators
+
+   IEnumerable<Using> GetAllCodeFileUsing(IEnumerable<CodeFile> namespaces);
+
+   Task<IEnumerable<Using>> GetAllCodeFileUsingAsync(IEnumerable<CodeFile> namespaces);
+
+   Task<IEnumerable<Using>> GetAllCodeFileUsingAsync(IEnumerable<CodeFile> namespaces, CancellationToken cancellationToken);
+
+   IEnumerable<Using> GetAllUsing(IEnumerable<Namespace> namespaces);
+
+   Task<IEnumerable<Using>> GetAllUsingAsync(IEnumerable<Namespace> namespaces);
+
+   Task<IEnumerable<Using>> GetAllUsingAsync(IEnumerable<Namespace> namespaces, CancellationToken cancellationToken);
+
+   #endregion
+}

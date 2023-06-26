2be80f01b72e0529400f0d55ffcb9c74b4180bce
Sándor Ács <sandormessi@hotmail.com>
2023-06-26 20:01:32 +0200

    Make namespace file scoped

--- Code/Other/CodeFileMergeApplication.Core/Implementation/Logic/CodeFileWriter.cs
+++ Code/Other/CodeFileMergeApplication.Core/Implementation/Logic/CodeFileWriter.cs
@@ -1,95 +1,94 @@
-﻿namespace CodeFileMergeApplication.Core.Implementation.Logic
+﻿namespace CodeFileMergeApplication.Core.Implementation.Logic;
+
+using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+using CodeFileMergeApplication.Core.Abstraction.Data;
+using CodeFileMergeApplication.Core.Abstraction.Logic;
+
+using CoreUtilities.Abstraction.FileSystem;
+
+public class CodeFileWriter : ICodeFileWriter
 {
-   using System;
-   using System.Text;
-   using System.Threading;
-   using System.Threading.Tasks;
+   #region Constants and Fields
 
-   using CodeFileMergeApplication.Core.Abstraction.Data;
-   using CodeFileMergeApplication.Core.Abstraction.Logic;
+   private const int InitialStringBuilderCapacity = 1024 * 4;
 
-   using CoreUtilities.Abstraction.FileSystem;
+   private readonly IFileWriter fileWriter;
 
-   public class CodeFileWriter : ICodeFileWriter
+   #endregion
+
+   #region Constructors and Destructors
+
+   public CodeFileWriter(IFileWriter fileWriter)
    {
-      #region Constants and Fields
-
-      private const int InitialStringBuilderCapacity = 1024 * 4;
-
-      private readonly IFileWriter fileWriter;
-
-      #endregion
-
-      #region Constructors and Destructors
-
-      public CodeFileWriter(IFileWriter fileWriter)
-      {
-         this.fileWriter = fileWriter ?? throw new ArgumentNullException(nameof(fileWriter));
-      }
-
-      #endregion
-
-      #region ICodeFileWriter Members
-
-      public void WriteCodeFile(CodeFile codeFile, string targetPath)
-      {
-         if (codeFile == null)
-         {
-            throw new ArgumentNullException(nameof(codeFile));
-         }
-
-         if (string.IsNullOrWhiteSpace(targetPath))
-         {
-            throw new ArgumentException("Value cannot be null or whitespace.", nameof(targetPath));
-         }
-
-         string codeFileText = CreateTextToWrite(codeFile);
-         fileWriter.WriteAllText(codeFileText, targetPath, Encoding.UTF8);
-      }
-
-      public Task WriteCodeFileAsync(CodeFile codeFile, string targetPath)
-      {
-         return WriteCodeFileAsync(codeFile, targetPath, CancellationToken.None);
-      }
-
-      public async Task WriteCodeFileAsync(CodeFile codeFile, string targetPath, CancellationToken cancellationToken)
-      {
-         if (codeFile == null)
-         {
-            throw new ArgumentNullException(nameof(codeFile));
-         }
-
-         if (string.IsNullOrWhiteSpace(targetPath))
-         {
-            throw new ArgumentException("Value cannot be null or whitespace.", nameof(targetPath));
-         }
-
-         string codeFileText = await Task.Factory.StartNew(() => CreateTextToWrite(codeFile), cancellationToken).ConfigureAwait(false);
-         await fileWriter.WriteAllTextAsync(codeFileText, targetPath, Encoding.UTF8, cancellationToken).ConfigureAwait(false);
-      }
-
-      #endregion
-
-      #region Methods
-
-      private static string CreateTextToWrite(CodeFile codeFile)
-      {
-         StringBuilder codeFileTextStringBuilder = new(InitialStringBuilderCapacity);
-
-         foreach (var codeFileUsing in codeFile.Usings)
-         {
-            codeFileTextStringBuilder.Append(codeFileUsing.UsingString);
-         }
-
-         foreach (var codeFileNamespace in codeFile.Namespaces)
-         {
-            codeFileTextStringBuilder.Append(codeFileNamespace.Definition);
-         }
-
-         string codeFileText = codeFileTextStringBuilder.ToString();
-         return codeFileText;
-      }
-
-      #endregion
+      this.fileWriter = fileWriter ?? throw new ArgumentNullException(nameof(fileWriter));
    }
+
+   #endregion
+
+   #region ICodeFileWriter Members
+
+   public void WriteCodeFile(CodeFile codeFile, string targetPath)
+   {
+      if (codeFile == null)
+      {
+         throw new ArgumentNullException(nameof(codeFile));
+      }
+
+      if (string.IsNullOrWhiteSpace(targetPath))
+      {
+         throw new ArgumentException("Value cannot be null or whitespace.", nameof(targetPath));
+      }
+
+      string codeFileText = CreateTextToWrite(codeFile);
+      fileWriter.WriteAllText(codeFileText, targetPath, Encoding.UTF8);
+   }
+
+   public Task WriteCodeFileAsync(CodeFile codeFile, string targetPath)
+   {
+      return WriteCodeFileAsync(codeFile, targetPath, CancellationToken.None);
+   }
+
+   public async Task WriteCodeFileAsync(CodeFile codeFile, string targetPath, CancellationToken cancellationToken)
+   {
+      if (codeFile == null)
+      {
+         throw new ArgumentNullException(nameof(codeFile));
+      }
+
+      if (string.IsNullOrWhiteSpace(targetPath))
+      {
+         throw new ArgumentException("Value cannot be null or whitespace.", nameof(targetPath));
+      }
+
+      string codeFileText = await Task.Factory.StartNew(() => CreateTextToWrite(codeFile), cancellationToken).ConfigureAwait(false);
+      await fileWriter.WriteAllTextAsync(codeFileText, targetPath, Encoding.UTF8, cancellationToken).ConfigureAwait(false);
+   }
+
+   #endregion
+
+   #region Methods
+
+   private static string CreateTextToWrite(CodeFile codeFile)
+   {
+      StringBuilder codeFileTextStringBuilder = new(InitialStringBuilderCapacity);
+
+      foreach (var codeFileUsing in codeFile.Usings)
+      {
+         codeFileTextStringBuilder.Append(codeFileUsing.UsingString);
+      }
+
+      foreach (var codeFileNamespace in codeFile.Namespaces)
+      {
+         codeFileTextStringBuilder.Append(codeFileNamespace.Definition);
+      }
+
+      string codeFileText = codeFileTextStringBuilder.ToString();
+      return codeFileText;
+   }
+
+   #endregion
 }

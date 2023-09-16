46649b5988c78dadfa14f5ab43696ce46d4fb066
Sándor Ács <sandormessi@hotmail.com>
2023-09-16 20:38:04 +0200

    Add skeleton taks classes to playground

---
+++ Code/Playground/CertificateTask/FifthTask.cs
@@ -0,0 +1,14 @@
+﻿namespace Playground.CertificateTask
+{
+   internal class FifthTask
+   {
+      #region Public Methods and Operators
+
+      public void Execute()
+      {
+         throw new NotImplementedException();
+      }
+
+      #endregion
+   }
+}

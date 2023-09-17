757a0481df1f7b7f7716d0239d7853b6d7fc4ee2
Sándor Ács <sandormessi@hotmail.com>
2023-09-17 20:20:25 +0200

    Clean playground

--- Code/Playground/Program.cs
+++ Code/Playground/Program.cs
@@ -1,14 +1,11 @@
 ﻿namespace Playground;
 
-using Playground.CertificateTask;
-
 internal class Program
 {
    #region Methods
 
    private static void Main(string[] args)
    {
-     
    }
 
    #endregion

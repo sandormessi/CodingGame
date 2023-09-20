6de61d3353b7017fe2a57190c1e88621399fd88f
Sándor Ács <sandormessi@hotmail.com>
2023-09-20 19:50:33 +0200

    Temperatures

---
+++ Code/Temperatures/TemperaturesGame.cs
@@ -0,0 +1,29 @@
+﻿namespace Temperatures;
+
+public class TemperaturesGame
+{
+   #region Methods
+
+   static void Main(string[] args)
+   {
+      int n = int.Parse(Console.ReadLine());
+      string[] inputs = Console.ReadLine().Split(' ');
+
+      List<int> temperatures = new();
+      for (int i = 0; i < n; i++)
+      {
+         temperatures.Add(int.Parse(inputs[i]));
+      }
+
+      long result = 0;
+      if (n > 0)
+      {
+         var ordered = temperatures.OrderBy(x => Math.Abs(0 - x)).ThenByDescending(x => x);
+         result = ordered.First();
+      }
+
+      Console.WriteLine(result);
+   }
+
+   #endregion
+}

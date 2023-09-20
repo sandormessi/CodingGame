3cfbd9f8e7abf53b1092f0f59242096fea81e8a3
Sándor Ács <sandormessi@hotmail.com>
2023-09-20 21:26:11 +0200

    Mime Types

---
+++ Code/MimeType/MimeTypeGame.cs
@@ -0,0 +1,44 @@
+﻿namespace MimeType;
+
+public class MimeTypeGame
+{
+   #region Methods
+
+   static void Main(string[] args)
+   {
+      int N = int.Parse(Console.ReadLine());
+      int Q = int.Parse(Console.ReadLine());
+
+      Dictionary<string, string> mimetypes = new Dictionary<string, string>(StringComparer.CurrentCultureIgnoreCase);
+      for (int i = 0; i < N; i++)
+      {
+         string[] inputs = Console.ReadLine().Split(' ');
+         string EXT = inputs[0];
+         string MT = inputs[1];
+
+         mimetypes.Add(EXT, MT);
+      }
+
+      for (int i = 0; i < Q; i++)
+      {
+         string FNAME = Console.ReadLine();
+         var extension = Path.GetExtension(FNAME);
+         if (string.IsNullOrWhiteSpace(extension))
+         {
+            Console.WriteLine("UNKNOWN");
+            continue;
+         }
+
+         if (mimetypes.TryGetValue(extension.TrimStart('.'), out var mimeType))
+         {
+            Console.WriteLine(mimeType);
+         }
+         else
+         {
+            Console.WriteLine("UNKNOWN");
+         }
+      }
+   }
+
+   #endregion
+}

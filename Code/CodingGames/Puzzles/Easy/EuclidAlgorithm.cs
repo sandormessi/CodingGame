b25b059693b123273dccd736fb79ff50d37c825e
Sándor Ács <sandormessi@hotmail.com>
2023-10-07 22:07:57 +0200

    EuclidAlgorithm

---
+++ Code/CodingGames/Puzzles/Easy/EuclidAlgorithm.cs
@@ -0,0 +1,43 @@
+﻿namespace CodingGames.Puzzles.Easy;
+
+public class EuclidAlgorithm
+{
+   #region Public Methods and Operators
+
+   public static void Main()
+   {
+      string[] inputs = ReadInput().Split(' ');
+
+      int a = int.Parse(inputs[0]);
+      int b = int.Parse(inputs[1]);
+
+      var gcd = Euclid(a, b);
+
+      Console.WriteLine($"GCD({a},{b})={gcd}");
+   }
+
+   #endregion
+
+   #region Methods
+
+   private static int Euclid(int a, int b)
+   {
+      var r = a % b;
+
+      Console.WriteLine($"{a}={b}*{a / b}+{r}");
+
+      if (r == 0)
+      {
+         return b;
+      }
+
+      return Euclid(b, r);
+   }
+
+   private static string ReadInput()
+   {
+      return Console.ReadLine() ?? throw new InvalidOperationException("There is not input.");
+   }
+
+   #endregion
+}

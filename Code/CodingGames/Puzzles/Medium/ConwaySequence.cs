64385fc118819ba2700b37fcd51410cabfa06731
Sándor Ács <sandormessi@hotmail.com>
2023-10-08 12:40:52 +0200

    Conway Sequence

---
+++ Code/CodingGames/Puzzles/Medium/ConwaySequence.cs
@@ -0,0 +1,59 @@
+﻿namespace CodingGames.Puzzles.Medium;
+
+public class ConwaySequence
+{
+   #region Public Methods and Operators
+
+   public static void Main()
+   {
+      int R = int.Parse(ReadInput());
+      int L = int.Parse(ReadInput());
+
+      int initialNumber = R;
+
+      List<int> actualLine = new(128) { initialNumber };
+
+      for (int i = 1; i < L; i++)
+      {
+         var groups = GetNumberGroupsOfLine(actualLine);
+         actualLine.Clear();
+         foreach (var group in groups)
+         {
+            actualLine.Add(group.Item2);
+            actualLine.Add(group.Item1);
+         }
+      }
+
+      Console.WriteLine(string.Join(' ', actualLine));
+   }
+
+   #endregion
+
+   #region Methods
+
+   private static IReadOnlyList<Tuple<int, int>> GetNumberGroupsOfLine(IReadOnlyList<int> line)
+   {
+      List<Tuple<int, int>> numberGroups = new();
+
+      for (int i = 0; i < line.Count;)
+      {
+         var actualCharacter = line[i];
+         var actualIndex = i;
+         while (i < line.Count && line[i] == actualCharacter)
+         {
+            i++;
+         }
+
+         numberGroups.Add(new Tuple<int, int>(actualCharacter, i - actualIndex));
+      }
+
+      return numberGroups;
+   }
+
+   private static string ReadInput()
+   {
+      return Console.ReadLine() ?? throw new InvalidOperationException("There is not input.");
+   }
+
+   #endregion
+}
